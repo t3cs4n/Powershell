e92bd9d2d9ea71c0f87d60c09ad3bbfeea14d32f
t3cs4n <mi.santi@icloud.com>
2025-06-13 23:52:46 +0200

    New File
    New File

---
+++ Intune/Get Hardware ID for Intune.ps1
@@ -0,0 +1,8 @@
+# Get Hardware ID for Intune
+
+
+md c:\HWID
+Set-Location c:\HWID
+Set-ExecutionPolicy Unrestricted
+Install-Script -Name Get-WindowsAutopilotInfo
+Get-WindowsAutopilotInfo.ps1 -OutputFile AutopilotHWID.csv
